f297bbb7a76a1dea9932aa8ca39a9a1a326680e0
Juaner_ <juanrape2000@gmail.com>
2025-09-29 04:32:39 -0500

    POI logic

--- Assets/Scripts/POI.cs
+++ Assets/Scripts/POI.cs
@@ -1,17 +1,23 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class POI : MonoBehaviour
 {
     [SerializeField] private GameObject visualizer;
     [SerializeField] private Transform root;
-
+    [SerializeField] private float tweenTime = 2;
+    [SerializeField] private LeanTweenType tweenType = LeanTweenType.easeInOutCubic;
+    
     private void Awake()
     {
         visualizer.SetActive(false);
+        GetComponentInChildren<Button>().onClick.AddListener(SetView);
     }
 
-    public void SetView()
+    private void SetView()
     {
-        Vector3.RotateTowards()
+        LeanTween.rotateY(root.gameObject, visualizer.transform.eulerAngles.y, tweenTime).setEase(tweenType);
+        Vector3 offset = visualizer.transform.position - root.position;
+        LeanTween.move(root.gameObject, Camera.main.transform.position + offset, tweenTime).setEase(tweenType);
     }
 }
