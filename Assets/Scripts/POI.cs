98946ce1b5fb9a82ffbfc251af3b66412b4722ca
Juaner_ <juanrape2000@gmail.com>
2025-09-29 07:15:02 -0500

    Move then rotate

--- Assets/Scripts/POI.cs
+++ Assets/Scripts/POI.cs
@@ -8,17 +8,53 @@ public class POI : MonoBehaviour
     [SerializeField] private float tweenTime = 2;
     [SerializeField] private LeanTweenType tweenType = LeanTweenType.easeInOutCubic;
 
+    private Camera _mainCamera;
+
     private void Awake()
     {
         visualizer.SetActive(false);
         GetComponentInChildren<Button>().onClick.AddListener(SetView);
     }
 
+    private void SetCamera()
+    {
+        _mainCamera = Camera.main;
+    }
+
     private void SetView()
     {
-        float angle = visualizer.transform.eulerAngles.y + Camera.main.transform.eulerAngles.y;
-        // LeanTween.rotateY(root.gameObject, angle, tweenTime).setEase(tweenType);
+        if (!_mainCamera)
+            SetCamera();
+
         Vector3 offset = root.position - visualizer.transform.position;
-        LeanTween.move(root.gameObject, Camera.main.transform.position + offset, tweenTime).setEase(tweenType);
+        LeanTween.move(root.gameObject, _mainCamera.transform.position + offset, tweenTime).setEase(tweenType)
+            .setOnComplete(() =>
+            {
+                float angle = visualizer.transform.localEulerAngles.y - Camera.main.transform.localEulerAngles.y;
+
+                // starting rotation
+                Quaternion startRot = root.rotation;
+
+                // final rotation: rotate root around pivot by targetAngle
+                Quaternion endRot = Quaternion.AngleAxis(angle, Vector3.up) * startRot;
+
+                // store pivot offset
+                Vector3 pivotOffset = root.position - visualizer.transform.position;
+
+                // tween with LeanTween
+                LeanTween.value(gameObject, 0f, 1f, tweenTime)
+                    .setEase(LeanTweenType.easeInOutSine)
+                    .setOnUpdate(t =>
+                    {
+                        // interpolate rotation
+                        Quaternion rot = Quaternion.Slerp(startRot, endRot, t);
+
+                        // apply rotation to offset
+                        Vector3 newPos = visualizer.transform.position +
+                                         rot * Quaternion.Inverse(startRot) * pivotOffset;
+
+                        root.SetPositionAndRotation(newPos, rot);
+                    });
+            });
     }
 }
